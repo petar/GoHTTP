46d66f833e8fb391b96265bd60d3bbd914072092
Petar Maymounkov <petar@math.mit.edu>
2011-02-16 18:07:41 -0500

    finished coding cookie

--- http/petar-cookie.go
+++ http/petar-cookie.go
@@ -19,18 +19,22 @@ import (
 // HTTP Request headers. In this case, only the Value@ field is
 // significant.
 type Cookie struct {
-	Value      string
-	Path       string
-	Domain     string
-	Comment    string
-	Version    uint
-	Expires    time.Time
-	ExpiresRaw string
-	MaxAge     int64 // Max age in nanoseconds
-	Secure     bool
-	HttpOnly   bool
-	Raw        string
-	Unparsed   []string // Raw text of unparsed attribute-value pairs
+	Value           string
+	Path            string
+	IncludePath     bool	// Whether to include a "$Path=" pair in "Cookie:" lines
+	Domain          string
+	IncludeDomain   bool	// Similar to IncludePath
+	Comment         string
+	IncludeComment  bool	// Similar to IncludePath
+	Version         uint
+	Expires         time.Time
+	ExpiresRaw      string
+	MaxAge          int64	// Max age in nanoseconds
+	Secure          bool
+	HttpOnly        bool
+	IncludeHttpOnly bool	// Similar to IncludePath
+	Raw             string
+	Unparsed        []string // Raw text of unparsed attribute-value pairs
 }
 
 // Cookies{} represents the collection of cookies found in a header.
@@ -42,47 +46,42 @@ type Cookies map[string]Cookie
 // the header h#, removes the successfully parsed values from the 
 // "Set-Cookie" key in h# and returns the parsed Cookie{}s.
 func extractSetCookies(h map[string][]string) *Cookies {
-	kk := new(Cookies)
-	sk, ok := h["Set-Cookie"]
+	cookies := new(Cookies)
+	lines, ok := h["Set-Cookie"]
 	if !ok {
-		return kk
+		return cookies
 	}
-	unparsed := make([]string, 0, 3)
-	for _, ktext := range sk {
-		parts := strings.Split(ktext, ";", -1)
+	unparsed_lines := make([]string, 0, 3)
+	for _, line := range lines {
+		parts := strings.Split(line, ";", -1)
 		if len(parts) == 0 {
-			unparsed = append(unparsed, ktext)
 			continue
 		}
 		nv := strings.Split(strings.TrimSpace(parts[0]), "=", 2) // Name=Value
 		if len(nv) != 2 {
-			unparsed = append(unparsed, ktext)
+			unparsed_lines = append(unparsed_lines, line)
 			continue
 		}
 		name, err := URLUnescape(nv[0])
 		if err != nil {
-			unparsed = append(unparsed, ktext)
+			unparsed_lines = append(unparsed_lines, line)
 			continue
 		}
 		value, err := URLUnescape(nv[1])
 		if err != nil {
-			unparsed = append(unparsed, ktext)
+			unparsed_lines = append(unparsed_lines, ktext)
 			continue
 		}
 		c := Cookie{
 			Value:    value,
 			MaxAge:   -1, // Not specified
-			Raw:      ktext,
+			Raw:      line,
 			Unparsed: make([]string, 0, 1),
 		}
 		for i := 1; i < len(parts); i++ {
 			av := strings.Split(strings.TrimSpace(parts[i]), "=", 2) // Attribute=Value
 			if len(av) == 1 {
-				arg, err := URLUnescape(av[0])
-				if err != nil {
-					c.Unparsed = append(c.Unparsed, parts[i])
-					continue
-				}
+				arg := av[0]
 				switch strings.ToLower(arg) {
 				case "secure":
 					c.Secure = true
@@ -92,11 +91,7 @@ func extractSetCookies(h map[string][]string) *Cookies {
 					c.Unparsed = append(c.Unparsed, parts[i])
 				}
 			} else if len(av) == 2 {
-				arg0, err := URLUnescape(av[0])
-				if err != nil {
-					c.Unparsed = append(c.Unparsed, parts[i])
-					continue
-				}
+				arg0 := av[0]
 				arg1, err := URLUnescape(av[1])
 				if err != nil {
 					c.Unparsed = append(c.Unparsed, parts[i])
@@ -132,7 +127,7 @@ func extractSetCookies(h map[string][]string) *Cookies {
 				case "version":
 					c.Version, err = strconv.Atoui(arg1)
 					if err != nil {
-						c.Version = 1
+						c.Version = 0
 						c.Unparsed = append(c.Unparsed, parts[i])
 						continue
 					}
@@ -143,14 +138,14 @@ func extractSetCookies(h map[string][]string) *Cookies {
 				}
 			}
 		} // Cookie attribute-value iteration
-		(*kk)[name] = c
+		(*cookies)[name] = c
 	} // header "Set-Cookie" value iteration
 	if len(unparsed) > 0 {
 		h["Set-Cookie"] = unparsed
 	} else {
 		h["Set-Cookie"] = nil, false
 	}
-	return kk
+	return cookies
 }
 
 // writeSetCookies() writes the wire representation of the set-cookies
@@ -164,6 +159,130 @@ func (kk *Cookies) writeSetCookies(w io.Writer) os.Error {
 		var version string
 		if c.Version > 1 {
 			version = "Version=" + strconv.Uitoa(c.Version) + "; "
+		}
+		var path string
+		if c.IncludePath && len(c.Path) > 0 {
+			path = "$Path=" + URLEscape(c.Path) + "; "
+		}
+		var domain string
+		if c.IncludeDomain && len(c.Domain) > 0 {
+			domain = "$Domain=" + URLEscape(c.Domain) + "; "
+		}
+		var httponly string
+		if c.IncludeHttpOnly && c.HttpOnly {
+			httponly = "$HttpOnly; "
+		}
+		var comment string
+		if c.IncludeComment && len(c.Comment) > 0 {
+			comment = "$Comment=" + URLEscape(c.Comment) + "; "
+		}
+		??
+		lines = append(lines,
+			"Cookie: "+value+version+domain+path+
+				expires+maxage+secure+httponly+comment)
+	}
+	sort.SortStrings(lines)
+	for _, l := range lines {
+		if _, err := io.WriteString(w, l); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// extractCookies() parses all "Cookie" values from
+// the header h#, removes the successfully parsed values from the 
+// "Cookie" key in h# and returns the parsed Cookie{}s.
+func extractCookies(h map[string][]string) *Cookies {
+	cookies := new(Cookies)
+	lines, ok := h["Cookie"]
+	if !ok {
+		return cookies
+	}
+	var unparsed_lines []string = make([]string, 0, 3)
+	for _, line := range lines {
+		parts := strings.Split(line, ";", -1)
+		if len(parts) == 0 {
+			continue
+		}
+		// Per-line attributes
+		var line_cookies = make(map[string]string)
+		var version int
+		var path string
+		var domain string
+		var comment string
+		var httponly bool
+		for i := 1; i < len(parts); i++ {
+			av := strings.Split(strings.TrimSpace(parts[i]), "=", 2) // Attribute=Value
+			if len(av) == 1 {
+				arg := av[0]
+				switch strings.ToLower(arg) {
+				case "$httponly":
+					httponly = true
+				}
+			} else if len(av) == 2 {
+				arg0 := av[0]
+				arg1, err := URLUnescape(av[1])
+				if err != nil {
+					continue
+				}
+				switch strings.ToLower(arg0) {
+				case "$version":
+					version, err = strconv.Atoui(arg1)
+					if err != nil {
+						version = 0
+						continue
+					}
+				case "$domain":
+					domain = arg1
+					// TODO: Add domain parsing
+				case "$path":
+					path = arg1
+					// TODO: Add path parsing
+				case "$comment":
+					comment = arg1
+				case "$httponly":
+					httponly = true
+				default:
+					line_cookies[arg0] = arg1
+				}
+			}
+		} // attribute-value iteration
+		if len(line_cookies) == 0 {
+			unparsed_lines = append(unparsed_line, line)
+		}
+		for n, v := range line_cookies {
+			(*cookies)[n] = Cookie{
+				Value:    v,
+				Path:     path,
+				Domain:   domain,
+				Comment:  comment,
+				Version:  version,
+				HttpOnly: httponly,
+				Raw:      line,
+			}
+		}
+	} // header "Cookie" line iteration
+
+	if len(unparsed_lines) > 0 {
+		h["Cookie"] = unparsed_lines
+	} else {
+		h["Cookie"] = nil, false
+	}
+	return cookies
+}
+
+// writeSetCookies() writes the wire representation of the cookies
+// to w#. Each cookie is written on a separate "Cookie: " line.
+// This choice is made because HTTP parsers tend to have a limit on
+// line-length, so it seems safer to place cookies on separate lines.
+func (kk *Cookies) writeCookies(w io.Writer) os.Error {
+	lines := make([]string, 0, len(*kk))
+	for n, c := range *kk {
+		var value string = n + "=" + URLEscape(c.Value) + "; "
+		var version string
+		if c.Version > 1 {
+			version = "$Version=" + strconv.Uitoa(c.Version) + "; "
 		}
 		var path string
 		if len(c.Path) > 0 {
@@ -193,9 +312,7 @@ func (kk *Cookies) writeSetCookies(w io.Writer) os.Error {
 		if len(c.Comment) > 0 {
 			comment = "Comment=" + URLEscape(c.Comment) + "; "
 		}
-		lines = append(lines,
-			"Set-Cookie: "+value+version+domain+path+
-				expires+maxage+secure+httponly+comment)
+		lines = append(lines, "Cookie: "+version+value+domain+path+httponly+comment)
 	}
 	sort.SortStrings(lines)
 	for _, l := range lines {
@@ -203,16 +320,5 @@ func (kk *Cookies) writeSetCookies(w io.Writer) os.Error {
 			return err
 		}
 	}
-	return nil
-}
-
-// extractCookies() parses all "Cookie" values from
-// the header h#, removes the successfully parsed values from the 
-// "Cookie" key in h# and returns the parsed Cookie{}s.
-func extractCookies(h map[string][]string) *Cookies {
-	return nil
-}
-
-func (kk *Cookies) writeCookies(w io.Writer) os.Error {
 	return nil
 }
---
+++ http/petar-cookie_test.go
@@ -0,0 +1,69 @@
+// Copyright 2010 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package http
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"reflect"
+	"testing"
+)
+
+/* extractSetCookie test */
+
+type extractSetCookieTest struct {
+	Raw  string
+	Parsed Cookies
+}
+
+var extractSetCookieTests = []extractSetCookieTest{ }
+
+func TestOK(t *testing.T) {
+	?
+}
+
+/*
+func TestReadResponse(t *testing.T) {
+	for i := range respTests {
+		tt := &respTests[i]
+		var braw bytes.Buffer
+		braw.WriteString(tt.Raw)
+		resp, err := ReadResponse(bufio.NewReader(&braw), tt.Resp.RequestMethod)
+		if err != nil {
+			t.Errorf("#%d: %s", i, err)
+			continue
+		}
+		rbody := resp.Body
+		resp.Body = nil
+		diff(t, fmt.Sprintf("#%d Response", i), resp, &tt.Resp)
+		var bout bytes.Buffer
+		if rbody != nil {
+			io.Copy(&bout, rbody)
+			rbody.Close()
+		}
+		body := bout.String()
+		if body != tt.Body {
+			t.Errorf("#%d: Body = %q want %q", i, body, tt.Body)
+		}
+	}
+}
+
+func diff(t *testing.T, prefix string, have, want interface{}) {
+	hv := reflect.NewValue(have).(*reflect.PtrValue).Elem().(*reflect.StructValue)
+	wv := reflect.NewValue(want).(*reflect.PtrValue).Elem().(*reflect.StructValue)
+	if hv.Type() != wv.Type() {
+		t.Errorf("%s: type mismatch %v vs %v", prefix, hv.Type(), wv.Type())
+	}
+	for i := 0; i < hv.NumField(); i++ {
+		hf := hv.Field(i).Interface()
+		wf := wv.Field(i).Interface()
+		if !reflect.DeepEqual(hf, wf) {
+			t.Errorf("%s: %s = %v want %v", prefix, hv.Type().(*reflect.StructType).Field(i).Name, hf, wf)
+		}
+	}
+}
+*/
