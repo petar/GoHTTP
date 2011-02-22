151741993b3c213be81aba5ec890990b2bec47c9
Petar Maymounkov <petar@math.mit.edu>
2011-02-22 13:22:54 -0500

    sync

--- http/client.go
+++ http/client.go
@@ -292,11 +292,7 @@ func Head(url string) (r *Response, err os.Error) {
 	if req.URL, err = ParseURL(url); err != nil {
 		return
 	}
-	url = req.URL.String()
-	if r, err = send(&req); err != nil {
-		return
-	}
-	return
+	return send(&req)
 }
 
 type nopCloser struct {
