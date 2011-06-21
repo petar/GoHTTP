ec030185fe9e780ca502bea96c681592c20ae6fe
Petar Maymounkov <petar@math.mit.edu>
2011-06-21 09:57:08 -0400

    add manual test of rpc subserver, including cookies values

--- server/rpc/rpc_test.go
+++ server/rpc/rpc_test.go
@@ -5,6 +5,7 @@
 package rpc
 
 import (
+	"fmt"
 	"os"
 	"testing"
 	"github.com/petar/GoHTTP/http"
@@ -13,38 +14,52 @@ import (
 
 type Service struct{}
 
-type In struct {
-	I0 int     "intv"
-	F0 float32 "floatv"
-	S0 string  "stringv"
-	B0 bool    "boolv"
-}
-
-type Out struct {
-	In In
-}
-
-func (s *Service) A(in *In, out *Out) os.Error {
-	out.In = *in
+func (s *Service) FS(args *ShortArgs, ret *ShortRet) os.Error {
+	fmt.Printf("Args--\n%#v\n--\n", args)
+	ret.Value = make(map[string]string)
+	ret.Value["key0"] = "val0"
 	return nil
 }
 
-func TestAPI(t *testing.T) {
-	srv, err := server.NewServerEasy("0.0.0.0:3232")
+func (s *Service) FL(args *LongArgs, ret *LongRet) os.Error {
+	fmt.Printf("Args--\n%#v\n--\n", args)
+	ret.Value = make(map[string][]string)
+	ret.Value["key0"] = []string{"val0"}
+	return nil
+}
+
+func (s *Service) FSC(args *ShortCookieArgs, ret *ShortSetCookieRet) os.Error {
+	fmt.Printf("Args--\n%#v\n--\n", args)
+	ret.Value = make(map[string]string)
+	ret.Value["key0"] = "val0"
+	return nil
+}
+
+func (s *Service) FLC(args *LongCookieArgs, ret *LongSetCookieRet) os.Error {
+	fmt.Printf("Args--\n%#v\n--\n", args)
+	if len(args.Cookies) > 0 {
+		fmt.Printf("Cookie0--\n%#v\n--\n", args.Cookies[0])
+	}
+	ret.Value = make(map[string][]string)
+	ret.Value["key0"] = []string{"val0"}
+	ret.SetCookies = make([]*http.Cookie, 1)
+	ret.SetCookies[0] = &http.Cookie{
+		Name:  "TestCookie",
+		Value: "TestCookieValue",
+	}
+	return nil
+}
+
+func TestRPC(t *testing.T) {
+	srv, err := server.NewServerEasy("0.0.0.0:3300")
 	if err != nil {
 		t.Fatalf("starting server: %s", err)
 	}
-	api := NewAPI()
-	err = api.Register(&Service{})
+	rpcs := NewRPC()
+	err = rpcs.RegisterName("s", &Service{})
 	if err != nil {
-		t.Fatalf("Service register: %s", err)
+		t.Fatalf("service register: %s", err)
 	}
-	srv.AddSub("/api/", api)
-	for {
-		q, err := srv.Read()
-		if err != nil {
-			t.Errorf("Problem: %s\n", err)
-		}
-		q.ContinueAndWrite(http.NewResponse404())
-	}
+	srv.AddSub("/api/", rpcs)
+	srv.Launch()
 }
